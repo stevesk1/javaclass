ac1ea445d8ea4db143ab03db03f980952846a4f7
stevesk1 <sounkes@yahoo.fr>
2020-07-18 01:57:43 -0400

    second commit

--- src/com/example/helloworld/HelloWorld.java
+++ src/com/example/helloworld/HelloWorld.java
@@ -1,7 +1,10 @@
 package com.example.helloworld;
 
 public class HelloWorld {
+    int continent = 7;
+    static int numberOfContinents = 7;
     public static void main(String[] args) {
+        int numberOfContinents = 7;
         short.class.arrayType();
     }
 
